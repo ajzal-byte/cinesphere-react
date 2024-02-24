2433f014919d2bb37f6a20cfb82795689d151e0f
ajzal-byte <ajzalaju10@gmail.com>
2024-02-24 14:11:45 +0530

    Search function implemented

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,21 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=15dd7d83'
 
-const movie1 = {
-  "Title": "Amazing Spiderman Syndrome",
-  "Year": "2012",
-  "imdbID": "tt2586634",
-  "Type": "movie",
-  "Poster": "N/A"
-}
-
 const App = ()=>{
+
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
   }
+
   useEffect(()=>{
-      searchMovies('spiderman');
-  }, [])
+      searchMovies('movie');
+  }, []);
+
   return (
     <div className="app">
       <h1>MovieLand</h1>
@@ -33,10 +29,11 @@ const App = ()=>{
       <div className="search">
         <input 
         placeholder="Search for movies"
-        value={'Superman'}
-        onChange={()=>{}} />
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
+
         <img src={SearchIcon} alt="searchIcon"
-        onClick={()=>{}} />
+        onClick={() => searchMovies(searchTerm)} />
       </div>
 
       {
